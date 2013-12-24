3edf23f2903a7a33e4b2b9e3d8a6549e80eb9b8c
Malinin Alexander <Malinin Alexander@localhost>
2013-12-24 21:57:45 +0400

    Trying to implement window resizing.

---
+++ Terminal/Source/TrueTypeTileset.hpp
@@ -0,0 +1,45 @@
+/*
+ * FreeTypeTileset.hpp
+ *
+ *  Created on: Nov 7, 2013
+ *      Author: cfyz
+ */
+
+#ifndef TRUETYPETILESET_HPP_
+#define TRUETYPETILESET_HPP_
+
+#include "Tileset.hpp"
+#include "Encoding.hpp"
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+namespace BearLibTerminal
+{
+	class TrueTypeTileset: public StronglyTypedReloadableTileset<TrueTypeTileset>
+	{
+	public:
+		TrueTypeTileset(TileContainer& container, OptionGroup& group);
+		~TrueTypeTileset();
+		void Dispose();
+		void Remove() override;
+		bool Save() override;
+		void Reload(TrueTypeTileset&& tileset) override;
+		Size GetBoundingBoxSize() override;
+		Type GetType() override;
+		bool Provides(uint16_t code) override;
+		void Prepare(uint16_t code) override;
+	private:
+		uint16_t m_base_code;
+		Size m_tile_size;
+		Size m_bbox_size;
+		std::unique_ptr<Encoding<char>> m_codepage;
+		Tile::Alignment m_alignment;
+		FT_Library m_font_library;
+		FT_Face m_font_face;
+		FT_Render_Mode m_render_mode;
+		bool m_monospace;
+	};
+}
+
+#endif /* TRUETYPETILESET_HPP_ */
