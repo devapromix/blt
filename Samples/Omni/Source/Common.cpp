73a92f159ae59de43cc9a21b23b589f67a9dfd86
Malinin Alexander <Malinin Alexander@localhost>
2014-07-07 17:40:25 +0400

    Fixed: X11 fullscreen mode cancelled by terminal size update.

---
+++ Samples/Omni/Source/Common.cpp
@@ -0,0 +1,34 @@
+/*
+ * Common.cpp
+ *
+ *  Created on: Nov 27, 2013
+ *      Author: cfyz
+ */
+
+#include "Common.hpp"
+
+#if defined(_WIN32)
+#include <Windows.h>
+void delay(int ms)
+{
+	Sleep(ms);
+}
+uint64_t GetTime()
+{
+	return timeGetTime();
+}
+#endif
+#if defined(__linux)
+#include <unistd.h>
+#include <sys/time.h>
+void delay(int ms)
+{
+	usleep(ms*1000);
+}
+uint64_t GetTime()
+{
+	timeval t;
+	gettimeofday(&t, nullptr);
+	return t.tv_sec*1000 + t.tv_usec/1000;
+}
+#endif
---
+++ Terminal/Source/DynamicTileset.hpp
@@ -0,0 +1,31 @@
+/*
+ * DynamicTileset.hpp
+ *
+ *  Created on: Oct 26, 2013
+ *      Author: Cfyz
+ */
+
+#ifndef DYNAMICTILESET_HPP_
+#define DYNAMICTILESET_HPP_
+
+#include "Tileset.hpp"
+
+namespace BearLibTerminal
+{
+	class DynamicTileset: public StronglyTypedReloadableTileset<DynamicTileset>
+	{
+	public:
+		DynamicTileset(TileContainer& container, OptionGroup& group);
+		void Remove();
+		bool Save();
+		void Reload(DynamicTileset&& tileset);
+		Size GetBoundingBoxSize();
+		Type GetType();
+		bool Provides(uint16_t code);
+		void Prepare(uint16_t code);
+	private:
+		Size m_tile_size;
+	};
+}
+
+#endif /* DYNAMICTILESET_HPP_ */
